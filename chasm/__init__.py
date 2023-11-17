75cb5f99f812d79b28366db6b9f35f3e43462ded
Ati Sharma <ati@agalmic.ltd>
2023-11-17 21:28:43 +0000

    more helpful bad-config error

--- chasm/__init__.py
+++ chasm/__init__.py
@@ -1,6 +1,14 @@
 import hy
-from chasm.lib import config
+import sys
+
+from chasm.lib import config, slurp
 
 if not (config("name") and config("passphrase") and config("server")):
-    print("You must specify name, passphrase and server in client.toml.")
+    print("""You must specify name, passphrase and server in client.toml.
+For example:
+    
+name = "Hero"
+passphrase = "Hero's super-secret passphrase"
+server = "tcp://chasm.run:25566"
+"""
     sys.exit(1)
