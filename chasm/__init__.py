bec8ae582e73e405b16d651382a90cca8c795d65
Ati Sharma <ati@agalmic.ltd>
2023-12-03 12:30:28 +0000

    fix: improve failed config handling, and readme

--- chasm/__init__.py
+++ chasm/__init__.py
@@ -3,25 +3,24 @@ import sys
 
 from chasm.lib import config, barf
 
-def _berate_config():
-    print("""You must specify a unique character name, a passphrase and the server in the file client.toml.
-For example, to connect to a world served on PORT:
-    
-name = "Hero"
+default_config = """name = "Hero"
 passphrase = "Hero's super-secret passphrase"
 server = "tcp://chasm.run:PORT"
+"""
 
-See https://chasm.run/worlds for what world to join.
-""")
+def _berate_config():
+    print("You must specify a unique character name, a passphrase and the server in the file client.toml.\nFor example, to connect to a world served on PORT:\n")
+    print(default_config)
+    print("See https://chasm.run/worlds for what world to join.")
 
 try:
     if not (config("name") and config("passphrase") and config("server")):
         _berate_config()
         sys.exit(1)
 except FileNotFoundError:
-    barf('name = "Player"\npassphrase = "your secret passphrase"\nserver = "https://chasm.run:WORLD_PORT"\n', "client.toml")
+    barf(default_config, "client.toml")
     print("""The file client.toml needs to be in the current directory.
-I've saved a template you can edit.
+I've saved a template client.toml you can edit.
 """)
     _berate_config()
 
